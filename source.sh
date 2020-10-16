c03011d532ce99c54e38dc8bb38e9f45711f6ae6
lschpmn <luis@lmc-productions.com>
2020-10-15 20:56:57 -0500

    typo

--- source.sh
+++ source.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )";
 BIN=$DIR/node_modules/.bin
 PATH=:$BIN:$DIR/bin:$PATH;
 alias start-client="ts-node index.ts";
-alias start-prod="ts-node ./server --prod >> log.txt & tial -f log.txt &";
+alias start-prod="ts-node ./server --prod >> log.txt & tail -f log.txt &";
 alias start-server="nodemon ./server/index.ts -w ./server/ </dev/null";
 alias start="start-client & start-server &";
