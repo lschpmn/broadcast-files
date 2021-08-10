4f81b0974f931c88de5864a10d40a5306d5868cd
lschpmn <luis@lmc-productions.com>
2021-08-10 13:58:42 -0500

    Added single start file

--- index.ts
+++ index.ts
@@ -1,26 +1,70 @@
+import { join } from 'path';
+import { readFile } from 'fs-extra';
+import { spawn } from 'child_process';
+import * as nodemon from 'nodemon';
 const getIncrementalPort = require('get-incremental-port');
-const _ = require('lodash');
 const webpackDevServer = require('webpack-dev-server');
 const webpack = require('webpack');
 const config = require('./webpack.config.js');
 
-(async function () {
-  const port = await getIncrementalPort(5000);
+const IS_PROD = process.argv.includes('--prod');
+const START_PORT = 5000;
+const TS_NODE_PATH = join(__dirname, 'node_modules', '.bin', 'ts-node.cmd');
 
-  const options = {
+(async function () {
+  const webpackPort = await getIncrementalPort(START_PORT);
+  const serverPort = await getIncrementalPort(START_PORT);
+
+  IS_PROD
+    ? startServer()
+    : startNodemon(serverPort);
+
+  startWebpack(webpackPort, serverPort);
+})();
+
+function startNodemon(port: number) {
+  console.log('Starting server');
+  nodemon({
+    args: ['--port', port.toString()],
+    ext: 'ts',
+    execMap: {
+      'ts': TS_NODE_PATH,
+    },
+    script: join(__dirname, 'server', 'index.ts'),
+    watch: [join(__dirname, 'server')],
+  });
+}
+
+function startServer() {
+  console.log('Starting server in prod mode');
+  const serverProcess = spawn(TS_NODE_PATH, ['server/index.ts', '--prod']);
+
+  serverProcess.stdout.on('data', data => console.log(data.toString()));
+  serverProcess.stderr.on('data', data => console.log(data.toString()));
+}
+
+function startWebpack(webpackPort, serverPort) {
+  console.log('starting webpack');
+  const compiler = webpack(config);
+  const server = new webpackDevServer(compiler, {
+    before: app => {
+      app.get(['/index.html', '/'], (req, res) => {
+        getIndexHtml(serverPort)
+          .then(html => res.send(html))
+          .catch(err => res.status(500).send(err));
+      });
+    },
     contentBase: './public',
     historyApiFallback: {
       disableDotRule: true,
     },
     hot: true,
-    host: 'localhost',
-  };
-
-  webpackDevServer.addDevServerEntrypoints(config, options);
-  const compiler = webpack(config);
-  const server = new webpackDevServer(compiler, options);
-
-  server.listen(port, '', () => {
-    console.log(`dev server listening on port ${port}`);
   });
-})();
+
+  server.listen(webpackPort, () => console.log(`starting on ${webpackPort}`));
+}
+
+async function getIndexHtml(port: number): Promise<string> {
+  const html = await readFile(join(__dirname, 'client', 'index.html'), 'utf8');
+  return html.replace('__PORT__ = 0', `__PORT__ = ${port}`);
+}
--- source.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )";
-BIN=$DIR/node_modules/.bin
-PATH=:$BIN:$DIR/bin:$PATH;
-alias start-client="ts-node index.ts";
-alias start-prod="ts-node ./server --prod >> log.txt & tail -f log.txt &";
-alias start-server="nodemon ./server/index.ts -w ./server/ </dev/null";
-alias start="start-client & start-server &";
