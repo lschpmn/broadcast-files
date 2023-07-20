6298e058bb5189b94eae3184ab99f414d74296b2
lschpmn <luis@lmc-productions.com>
2023-07-20 11:08:22 -0500

    Moved over command line arguments

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,6 @@ import ffmpeg from 'fluent-ffmpeg';
 import { read } from 'fs-jetpack';
 import { createServer } from 'https';
 import { join } from 'path';
-import process from 'process';
 import { Server, Socket } from 'socket.io';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
@@ -14,10 +13,9 @@ import config from '../webpack.config';
 import { DirectoriesRouter } from './Directories';
 import { initDB } from './lib/db';
 import { setupUsers, UsersRouter } from './Users';
-import { log } from './lib/utils';
+import { getCommandLineArguments, log } from './lib/utils';
 
-const IS_PROD = process.argv.includes('--prod');
-const PORT = process.argv.includes('--port') ? process.argv[process.argv.indexOf('--port') + 1] : 5000;
+const { PORT, PROD } = getCommandLineArguments();
 
 ffmpeg.setFfmpegPath(join(__dirname, '..', 'bin', 'ffmpeg.exe'));
 ffmpeg.setFfprobePath(join(__dirname, '..', 'bin', 'ffprobe.exe'));
@@ -26,60 +24,60 @@ let app: Express;
 // command to make ssl cert
 // openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ./bin/server.key -out ./bin/server.cert
 async function startServer() {
-  app = express();
-  const server = createServer({
-    key: read(join(__dirname, '../bin/server.key')),
-    cert: read(join(__dirname, '../bin/server.cert')),
-  }, app);
-  await initDB();
+    app = express();
+    const server = createServer({
+        key: read(join(__dirname, '../bin/server.key')),
+        cert: read(join(__dirname, '../bin/server.cert')),
+    }, app);
+    await initDB();
 
-  // Webpack
-  const compiler = webpack(config);
-  app.use(webpackDevMiddleware(compiler, {}));
-  app.use(webpackHotMiddleware(compiler));
+    // Webpack
+    const compiler = webpack(config);
+    app.use(webpackDevMiddleware(compiler, {}));
+    app.use(webpackHotMiddleware(compiler));
 
-  const io = new Server(server, { maxHttpBufferSize: 1024 * 1024 * 500 /*500MB*/ });
-  io.on('connection', (socket: Socket) => {
-    log('client connected');
+    const io = new Server(server, { maxHttpBufferSize: 1024 * 1024 * 500 /*500MB*/ });
+    io.on('connection', (socket: Socket) => {
+        log('client connected');
 
-    socket.on('disconnect', () => log('client disconnected'));
-  });
+        socket.on('disconnect', () => log('client disconnected'));
+    });
 
-  app.use((req, res, next) => {
-    const ip = req.header('x-real-ip'); // ip address from nginx
-    log(`ip - ${ip || req.ip} - url - ${req.url}`);
-    next();
-  });
+    app.use((req, res, next) => {
+        const ip = req.header('x-real-ip'); // ip address from nginx
+        log(`ip - ${ip || req.ip} - url - ${req.url}`);
+        next();
+    });
 
-  !IS_PROD && app.use(cors({
-    allowedHeaders: ['x-crypto-iv', 'x-crypto-key'],
-    credentials: true,
-    exposedHeaders: 'x-crypto-iv',
-    origin: true,
-  }));
+    !PROD && app.use(cors({
+        allowedHeaders: ['x-crypto-iv', 'x-crypto-key'],
+        credentials: true,
+        exposedHeaders: 'x-crypto-iv',
+        origin: true,
+    }));
 
-  app.use(cookieParser());
-  app.use(express.text());
-  app.use(express.static(join(__dirname, '..', 'public')));
+    app.use(cookieParser());
+    app.use(express.text());
+    app.use(express.static(join(__dirname, '..', 'public')));
 
-  await setupUsers();
+    await setupUsers();
 
-  app.use('/api', UsersRouter);
-  app.use('/api', DirectoriesRouter);
-  app.use('/api', (req, res) => res.status(404).end());
+    app.use('/api', UsersRouter);
+    app.use('/api', DirectoriesRouter);
+    app.use('/api', (req, res) => res.status(404).end());
 
-  app.use((err, req, res, next) => {
-    log(err);
-    log(err.stack);
-    res.status(500).send(err);
-  });
+    app.use((err, req, res, next) => {
+        log(err);
+        log(err.stack);
+        res.status(500).send(err);
+    });
 
-  app.use((req: Request, res: Response) => {
-    log(`404 - sending index.html - ${req.url}`);
-    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
-  });
+    app.use((req: Request, res: Response) => {
+        log(`404 - sending index.html - ${req.url}`);
+        res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+    });
 
-  server.listen(PORT, () => log(`started server at https://localhost:${PORT}`));
+    server.listen(PORT, () => log(`started server at https://localhost:${PORT}`));
 }
 
 startServer().catch(log);
--- server/lib/utils.ts
+++ server/lib/utils.ts
@@ -1,4 +1,20 @@
 import dayjs from 'dayjs';
 
+export const getCommandLineArguments = (): { PORT: number, PROD: boolean } => {
+    const { argv } = process;
+    const portIndex = argv.indexOf('--port');
+    const port = portIndex > -1 ? +argv[portIndex + 1] : 0;
+
+    if (!port) {
+        console.error('--port must be defined!');
+        process.exit();
+    }
+
+    return {
+        PORT: port,
+        PROD: argv.includes('--prod'),
+    };
+};
+
 export const log = (message: string) =>
     console.log(`${dayjs().format('hh:mm:ss.SSS A ddd MMM DD YYYY')} - ${message}`);
