86c246c3b3a6aab6a0b934563d0d8688ad060455
lschpmn <luis@lmc-productions.com>
2023-07-24 07:57:19 -0500

    Added icons and home icon for path header

--- client/components/DirectoryWindow/PathHeader.tsx
+++ client/components/DirectoryWindow/PathHeader.tsx
@@ -1,5 +1,5 @@
+import HomeIcon from '@mui/icons-material/Home';
 import { Button } from '@mui/material';
-import { grey } from '@mui/material/colors';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -8,15 +8,15 @@ const PathHeader = () => {
   const routes = pathname.split('/').slice(1);
 
   return <div style={{ display: 'flex' }}>
-    <Link to="/">
+    <Link to="/" style={{ display: 'flex' }}>
       <Button style={{ color: 'white' }}>
-        <h2>~</h2>
+        <HomeIcon/>
       </Button>
     </Link>
 
     {routes.map((route, i) =>
       <React.Fragment key={i}>
-        <h2 style={{ padding: '6px 8px' }}>/</h2>
+        <h2 style={{ color: 'white', padding: '6px 8px' }}>/</h2>
         <Link to={'/' + routes.slice(0, i + 1).join('/')}>
           <Button style={{ color: 'white' }}>
             <h2>{decodeURIComponent(route)}</h2>
