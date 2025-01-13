6226ca40a842a6295d719f9a838b12b87dcbba99
lschpmn <luis@thechapman.site>
2025-01-12 23:34:28 -0600

    Responding to mouse movement and hiding cursor when video is playing

--- client/components/VideoView/ControlsComponent/index.tsx
+++ client/components/VideoView/ControlsComponent/index.tsx
@@ -1,13 +1,15 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import { debounce } from 'lodash';
 import React, { useEffect, useState } from 'react';
 import { getTimeStr } from '../../../lib/utils';
 import AutoplayComponent from './AutoplayComponent';
 import PlayComponent from './PlayComponent';
 import SliderComponent from './SliderComponent';
 
-const OPACITY = 0.75;
+const OPACITY = 0.95;
 const FRAMES = 33;
-const RATE = OPACITY / FRAMES / 2;
+const TIMEOUT = 2000;
+const RATE = OPACITY / FRAMES / TIMEOUT * 1000;
 
 type Props = {
   currentTime: number,
@@ -18,13 +20,17 @@ type Props = {
 };
 
 const ControlsComponent = ({ currentTime, duration, isPlaying, setTime, video }: Props) => {
+  const [isMouseMoving, setIsMouseMoving] = useState(false);
   const [opacity, setOpacity] = useState(OPACITY);
 
   useEffect(() => {
-    if (isPlaying) {
+    if (isPlaying && !isMouseMoving) {
       const intervalId = setInterval(() => {
         setOpacity(p => {
-          if (p < 0) clearInterval(intervalId)
+          if (p < 0) {
+            clearInterval(intervalId);
+            video && (video.style.cursor = 'none');
+          }
           return p - RATE;
         });
       }, FRAMES);
@@ -32,13 +38,22 @@ const ControlsComponent = ({ currentTime, duration, isPlaying, setTime, video }:
       return () => clearInterval(intervalId);
     } else {
       setOpacity(OPACITY);
+      video && (video.style.cursor = 'auto');
     }
-  }, [isPlaying]);
+  }, [isPlaying, isMouseMoving]);
 
-  if (opacity < 0) return <div />;
+  useEffect(() => {
+    const start = debounce(() => setIsMouseMoving(true), TIMEOUT, { leading: true, trailing: false });
+    const end = debounce(() => setIsMouseMoving(false), TIMEOUT, { leading: false, trailing: true });
+
+    const lisenter = () => { start(); end(); }
+
+    document.addEventListener('mousemove', lisenter);
+    return () => document.removeEventListener('mousemove', lisenter);
+  }, []);
 
   return (
-    <AppBar position="absolute" style={{ bottom: 0, opacity: opacity, top: 'auto' }}>
+    <AppBar position="absolute" style={{ bottom: 0, opacity: opacity, top: 'auto', zIndex: opacity > 0 ? 100 : -100 }}>
       <Toolbar style={{ display: 'flex', flexDirection: 'column' }}>
         <SliderComponent currentTime={currentTime} duration={duration} setTime={setTime}/>
         <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
